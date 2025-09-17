a10f62ded10d382ecfffd91bc46fa57886b93e5e
agastya_pulgam <pulgamagastya@gmail.com>
2025-09-17 12:17:13 +0530

    added class modificaction feature

--- class_modify.py
+++ class_modify.py
@@ -41,7 +41,44 @@ def remove_class(standard,division):
 
 # BELOW GIVEN FUNCTION IS USE TO EDIT CLASS DETAILS, SUCH AS CLASS TEACHER,TOTAL STRENGTH, STANDARD AND DIVISION AN 
 # AN EXAMPLE CASE OF THIS IS WHEN A CLASS IS PROMOTED TO A HIGHER GRADE, INSTEAD OF CREATING A NEW CLASS FOR PRE-EXISTNG STUDENTS,
-# THE TEACHER CAN JUST EDIT THE CLASS STANDARD, SAVING TIME FROM FILLING OUT HE DETAILS OF THE CLASS AGAIN
+# THE TEACHER CAN JUST EDIT THE CLASS STANDARD, SAVING TIME FROM FILLING OUT THE DETAILS OF THE CLASS AGAIN
 
 
+def modify_class(class_id, new_standard=None, new_division=None, new_strength=None):
 
+    try:
+        connection=get_connection()
+        cur=connection.cursor(buffered=True)
+        fields=[]
+        values=[]
+        connection.start_transaction()
+        if new_standard:
+
+            fields.append('standard=%s')
+            values.append(new_standard)
+
+        elif new_division:
+            
+            fields.append('division=%s')
+            values.append(new_division)
+
+        elif new_strength:
+
+            fields.append('total_strength=%s')
+            values.append(new_strength)
+
+        if not fields:
+            return {'status':'error','message':'no fields to update'}
+
+        cur.execute(f'update classes set{', '.join(fields)} where class_id = %s',(values.append(class_id)))
+        connection.commit()
+        
+        return {'status':'ok','updated':cur.rowcount}
+    
+    except Exception as e:
+        connection.rollback()
+        return {'status':'error','message':str(e)}
+    
+    finally:
+        cur.close()
+        connection.close()
